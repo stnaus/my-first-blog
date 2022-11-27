30395406617dc0b56f2a0fbb216790735da3581c
stnaus <st.al.gusev@gmail.com>
2022-11-27 21:50:33 +0300

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
+# Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -16,4 +16,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-# Create your models here.
